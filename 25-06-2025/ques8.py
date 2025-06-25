524e6c0c884b56c8ba56d7a10b143eb7595ee3de
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2025-06-25 15:14:55 +0530

    Create ques8.py

---
+++ 25-06-2025/ques8.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, l, w):
+        self.length = l
+        self.width = w
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+r = Rectangle(10, 5)
+print("Area:", r.area())
+print("Perimeter:", r.perimeter())
